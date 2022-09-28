0cef6ef9deba61a02717d60c3fb9517d750982db
rkskekzzz <wkdlfflxh@naver.com>
2022-09-28 15:24:51 +0900

    ✨ content panel 내부 view 생성

---
+++ extension/src/contentScript/components/content/Recommand.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Recommand = () => {
+  return <div>Recommand</div>;
+};
+
+export default Recommand;
--- extension/src/contentScript/components/content/index.ts
+++ extension/src/contentScript/components/content/index.ts
@@ -1,3 +1,6 @@
 import ContentPanelNavigator from './ContentPanelNavigator';
+import Profile from './Profile';
+import Rank from './Rank';
+import Recommand from './Recommand';
 
-export { ContentPanelNavigator };
+export { ContentPanelNavigator, Profile, Rank, Recommand };
