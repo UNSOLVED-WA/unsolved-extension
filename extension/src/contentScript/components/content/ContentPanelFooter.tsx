9a2c523c1e8269345840bd744cd9f0258c24205c
rkskekzzz <wkdlfflxh@naver.com>
2022-09-30 17:02:34 +0900

    ✨ ContentPanel footer 추가

---
+++ extension/src/contentScript/components/content/ContentPanelFooter.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+
+  padding: 5px 0;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: start;
+  align-items: center;
+
+  h1 {
+    padding: 0px;
+    margin: 0px;
+    font-size: 12px;
+    font-weight: 600;
+  }
+
+  .links > a {
+    font-size: 8px;
+  }
+`;
+
+const ContentPanelFooter = () => {
+  return (
+    <Container>
+      <h1>@unsolved.wa</h1>
+      <div className="links">
+        <a href="https://github.com/unsolved-wa">Github</a>
+        <span> | </span>
+        <a href="https://google.com">blog</a>
+        <span> | </span>
+      </div>
+    </Container>
+  );
+};
+
+export default ContentPanelFooter;
--- extension/src/contentScript/components/content/index.ts
+++ extension/src/contentScript/components/content/index.ts
@@ -1,7 +1,8 @@
 import ContentPanelNavigator from './ContentPanelNavigator';
 import ContentPanelHeader from './ContentPanelHeader';
+import ContentPanelFooter from './ContentPanelFooter';
 import Profile from './Profile';
 import Rank from './Rank';
 import Recommand from './Recommand';
 
-export { ContentPanelNavigator, ContentPanelHeader, Profile, Rank, Recommand };
+export { ContentPanelNavigator, ContentPanelHeader, ContentPanelFooter, Profile, Rank, Recommand };
