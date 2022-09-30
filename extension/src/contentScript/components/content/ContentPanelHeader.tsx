1c9b0520e0b2d6811d9e06a34e1915f7d38da2c3
rkskekzzz <wkdlfflxh@naver.com>
2022-09-30 12:54:05 +0900

    💄 panel header background 주석

--- extension/src/contentScript/components/content/ContentPanelHeader.tsx
+++ extension/src/contentScript/components/content/ContentPanelHeader.tsx
@@ -15,8 +15,8 @@ const Container = styled.div`
   padding-left: 60px;
   border-radius: 15px 15px 0 0 !important;
 
-  background-color: rgba(255, 255, 255, 0.5);
-  backdrop-filter: blur(60px);
+  /* background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(60px); */
 
   h1 {
     padding: 0px;
