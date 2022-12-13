3184382d28ce6d194b683401291c4e1cd14de03f
mike2ox <firefinger07@gmail.com>
2022-12-13 14:08:40 +0900

    Chore : update mockapi, add api description

--- extension/src/api/mockapi.ts
+++ extension/src/api/mockapi.ts
@@ -3,7 +3,7 @@ import { ProblemResponse } from '../@types/Problem';
 import { Ranking } from '../@types/Ranking';
 
 import { dummyUnsolvedUser } from '../test/dummy/dummyUser';
-import { dummyProblem, dummyProblems } from '../test/dummy/dummyProblem';
+import { dummyUpdateProblems, dummyProblems } from '../test/dummy/dummyProblem';
 import { dummyAllRanking, dummyMonthRanking } from '../test/dummy/dummyRanking';
 
 function objectToPromise<T>(obj: T): Promise<T> {
@@ -22,10 +22,10 @@ const UserService = {
 const ProblemService = {
   updateUnsolvedProblems: async (userId: number, problemNumber: number): Promise<ProblemResponse[]> => {
     console.log('mockAPI: updateUnsolvedProblems : ', userId, problemNumber);
-    return objectToPromise(dummyProblems);
+    return objectToPromise(dummyUpdateProblems);
   },
-  getUnsolvedProblems: async (bojId: string): Promise<ProblemResponse[]> => {
-    console.log('mockAPI: getUnsolvedProblems : ', bojId);
+  getUnsolvedProblems: async (bojId: string, tier: string): Promise<ProblemResponse[]> => {
+    console.log('mockAPI: getUnsolvedProblems : ', bojId, tier);
     return objectToPromise(dummyProblems);
   },
 };
