ea79a51b69b27a9a524d600815039ad427fcf297
mike2ox <firefinger07@gmail.com>
2022-12-13 14:16:26 +0900

    Feat : 추천 문제 리스트 추가(rough)

--- extension/src/contentScript/components/content/RecommandView.tsx
+++ extension/src/contentScript/components/content/RecommandView.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ProblemResponse } from '../../../@types/Problem';
+import { ContentBox } from '../../common';
 
 const RecommandView = () => {
-  return <div>Recommand</div>;
+  const [recommand, setRecommand] = useState<ProblemResponse[]>([]);
+
+  const redirectProblemInfo = (data: number) => (window.location.href = `https://www.acmicpc.net/problem/${data}`);
+
+  useEffect(() => {
+    // TODO: 추천 문제 갖고오는 message
+    chrome.runtime.sendMessage({ message: 'fetchRecommand', type: 'async', data: { teamId: '1', tier: '1' } }, (response) => {
+      if (response.state === 'success') {
+        setRecommand(response.data);
+      }
+    });
+  }, []);
+
+  return (
+    <div className='panel-contents'>
+      {recommand.map((problem) => (
+        <ContentBox key={problem.problemId} color='bronze'>
+          <div onClick={() => redirectProblemInfo(problem.problemId)}>
+            <div>{problem.problemId}</div>
+            <div>{problem.problemTitle}</div>
+            <div>{problem.tier}</div>
+          </div>
+        </ContentBox>
+      ))}
+    </div>
+  );
 };
 
 export default RecommandView;
