f77628fa34e18b9827cb46ce6d4d295e4f5f0902
mike2ox <firefinger07@gmail.com>
2022-12-13 13:39:01 +0900

    Feat : 클릭한 유저의 백준 프로필로 이동

--- extension/src/contentScript/components/content/RankingView.tsx
+++ extension/src/contentScript/components/content/RankingView.tsx
@@ -5,7 +5,10 @@ import { ContentBox } from '../../common';
 const RankingView = () => {
   const [ranking, setRanking] = useState<Ranking[]>([]);
 
+  const redirectUserInfo = (bojId: string) => (window.location.href = `https://www.acmicpc.net/user/${bojId}`);
+
   useEffect(() => {
+    // TODO: 여러 그룹일 경우 data에 넣을 teamId 설정 필요
     chrome.runtime.sendMessage({ message: 'fetchRanking', type: 'async', data: '1' }, (response) => {
       if (response.state === 'success') {
         setRanking(response.data);
@@ -17,8 +20,10 @@ const RankingView = () => {
     <div className='panel-contents'>
       {ranking.map((user) => (
         <ContentBox key={user.teamName + user.bojId} color='bronze'>
-          <div>{user.bojId}</div>
-          <div>{user.score}</div>
+          <div onClick={() => redirectUserInfo(user.bojId)}>
+            <div>{user.bojId}</div>
+            <div>{user.score}</div>
+          </div>
         </ContentBox>
       ))}
     </div>
