88de84850761f5f1fba72ad3876c0faa5249d1e9
mike2ox <firefinger07@gmail.com>
2022-12-23 17:21:01 +0900

    Design : 추천 문제 아이템 디자인 변경

--- extension/src/contentScript/components/content/RecommandView.tsx
+++ extension/src/contentScript/components/content/RecommandView.tsx
@@ -9,6 +9,12 @@ const RecommandView = () => {
     chrome.runtime.sendMessage({ message: 'toRedirectProblem', type: 'sync', data: problemId });
   };
 
+  const submitPassedProblem = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('문제 통과 제출');
+  };
+
   useEffect(() => {
     // TODO: 추천 문제 갖고오는 message
     chrome.runtime.sendMessage({ message: 'fetchRecommand', type: 'async', data: { teamId: '1', tier: '1' } }, (response) => {
@@ -22,10 +28,20 @@ const RecommandView = () => {
     <div className='panel-contents'>
       {recommand.map((problem) => (
         <ContentBox key={problem.problemId} color='bronze'>
-          <div onClick={() => redirectProblemInfo(problem.problemId)}>
-            <div>{problem.problemId}</div>
-            <div>{problem.problemTitle}</div>
-            <div>{problem.tier}</div>
+          <div
+            onClick={() => {
+              redirectProblemInfo(problem.problemId);
+            }}
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
+            <div style={{ minWidth: '20px', backgroundColor: 'gray', borderRadius: '50%', textAlign: 'center' }}>{problem.tier}</div>
+            <div style={{ margin: '0 5px' }}>
+              <span style={{ display: 'block' }}>{problem.problemId}</span>
+              <span style={{ display: 'block', width: '180px', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
+                {problem.problemTitle}
+              </span>
+            </div>
+            <button onClick={submitPassedProblem}>제출</button>
           </div>
         </ContentBox>
       ))}
