1b6039086864ff9c4c2982b35d17804963e13091
mike2ox <firefinger07@gmail.com>
2022-12-26 16:20:05 +0900

    Feat : 제출후 문제 통과여부 확인

--- extension/src/test/solvedTest.ts
+++ extension/src/test/solvedTest.ts
@@ -1,18 +1,26 @@
-if (window.location.pathname.includes("/submit")) {
-  const button = document.querySelector("#submit_button");
+if (window.location.pathname.includes('/submit')) {
+  const button = document.querySelector('#submit_button');
   if (button) {
-    button.addEventListener("click", () => {
-      const problemNumber = window.location.href.split("/").pop();
+    button.addEventListener('click', () => {
+      const splitedURL = window.location.href.split('/');
+      const problemNumber = splitedURL[splitedURL.indexOf('submit') + 1];
       chrome.storage.local.set({ submit: problemNumber });
     });
   }
 }
 
-if (window.location.pathname.includes("/status")) {
-  chrome.runtime.sendMessage(
-    { message: "submit", type: "async" },
-    (response) => {
-      console.log(response);
+if (window.location.pathname.includes('/status')) {
+  const MAX_CHECK_TIME = 30000;
+  const CHECK_INTERVAL = 1000;
+
+  const result = document.querySelector('.result');
+
+  const checkPassed = () => {
+    if (result.children[0].innerHTML === '맞았습니다!!') {
+      clearInterval(monitoring);
+      clearTimeout(limitTime);
     }
-  );
+  };
+  const limitTime = setTimeout(checkPassed, MAX_CHECK_TIME);
+  const monitoring = setInterval(checkPassed, CHECK_INTERVAL);
 }
