0897c7f1db7db83301539373f0e4541e3a4c1870
rkskekzzz <wkdlfflxh@naver.com>
2022-12-29 14:22:24 +0900

    🔖 채점 상태 타입 추가

--- extension/src/@types/index.ts
+++ extension/src/@types/index.ts
@@ -1,3 +1,5 @@
 export const ICONS = ['face', 'star', 'recommend', 'close', 'refresh', 'edit_square'] as const;
 export type ICON = typeof ICONS[number];
 export type URL = string;
+
+export type SCORING_STATE = 'DEFAULT' | 'RUNNING' | 'CORRECT' | 'WRONG' | 'TIMEOUT' | 'ERROR';
