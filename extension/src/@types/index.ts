4c893aac6b9f0d46586cd5f1ee5225121fa88e57
rkskekzzz <wkdlfflxh@naver.com>
2022-12-29 20:30:58 +0900

    🔖 크롬 스토리지에 저장가능한 타입 지정

--- extension/src/@types/index.ts
+++ extension/src/@types/index.ts
@@ -1,5 +1,15 @@
+import { SolvedUser } from './SolvedUser';
+
 export const ICONS = ['face', 'star', 'recommend', 'close', 'refresh', 'edit_square'] as const;
 export type ICON = typeof ICONS[number];
 export type URL = string;
 
 export type SCORING_STATE = 'DEFAULT' | 'RUNNING' | 'CORRECT' | 'WRONG' | 'TIMEOUT' | 'ERROR';
+
+export type STORAGE_VALUE = {
+  solvedUser: string;
+  badge: string;
+  hideButton: boolean;
+  problenNo: number;
+  isClicked: boolean;
+};
