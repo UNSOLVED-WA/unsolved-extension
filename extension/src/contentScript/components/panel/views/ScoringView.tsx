4f2de8c1f5514f368a31d5f7aeca4cdcee1ef933
rkskekzzz <wkdlfflxh@naver.com>
2022-12-30 21:22:35 +0900

    🔖 scoringState 타입 지정

--- extension/src/contentScript/components/panel/views/ScoringView.tsx
+++ extension/src/contentScript/components/panel/views/ScoringView.tsx
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Scoring } from '../../../../utils';
 import { ContentBox } from '../../../common';
+import { SCORING_STATE } from '../../../../@types';
 
 const ScoringView = () => {
-  const [message, setMessage] = useState('');
+  const [scoringState, setScoringState] = useState<{ key: SCORING_STATE; value: string }>({ key: 'DEFAULT', value: '' });
 
   useEffect(() => {
-    Scoring.getMessageByState().then((message) => {
-      setMessage(message);
+    Scoring.getMessageByState().then((result) => {
+      setScoringState(result);
     });
     const ScoringStateHandler = (changes: { [key: string]: chrome.storage.StorageChange }) => {
       if (changes.scoringState) {
-        Scoring.getMessageByState(changes.scoringState.newValue).then((message) => {
-          setMessage(message);
+        Scoring.getMessageByState(changes.scoringState.newValue).then((result) => {
+          setScoringState(result);
         });
       }
     };
     chrome.storage.onChanged.addListener(ScoringStateHandler);
-    return () => chrome.storage.onChanged.removeListener(ScoringStateHandler);
+    () => chrome.storage.onChanged.removeListener(ScoringStateHandler);
   }, []);
 
-  useEffect(() => {
-    console.log(message);
-  }, [message]);
   return (
     <div className='panel-contents' style={{ height: '100%' }}>
       <ContentBox fullHeight>
-        <div>{message}</div>
+        <div>{scoringState.value}</div>
       </ContentBox>
     </div>
   );
