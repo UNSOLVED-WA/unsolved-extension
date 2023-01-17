48f0f4b728c3db3b7c89c501eb8bcb6cf75a40dd
rkskekzzz <wkdlfflxh@naver.com>
2023-01-17 18:29:55 +0900

    ✨ zip 파일 생성하는 스크립트 추가, bundle analyzer 실행 안되게 수정

--- extension/webpack.prod.js
+++ extension/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+// if environment 'ZIP' is set, not run webpack bundle analyzer
+if (!process.env.UWZIP) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  common.plugins.push(new BundleAnalyzerPlugin());
+}
+
 module.exports = merge(common, {
   mode: 'production',
 });
