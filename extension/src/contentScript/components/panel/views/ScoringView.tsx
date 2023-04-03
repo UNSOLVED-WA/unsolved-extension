16d3e09a269dd16391608069db917e8c1304ec3d
rkskekzzz <wkdlfflxh@naver.com>
2023-04-03 23:52:10 +0900

    🐛 profileview에만 fallbackAction 적용되어 있던 문제 수정

--- extension/src/contentScript/components/panel/views/ScoringView.tsx
+++ extension/src/contentScript/components/panel/views/ScoringView.tsx
@@ -4,10 +4,10 @@ import { ScoringBox } from './boxes';
 import { useTeam } from '../../../hooks';
 
 const ScoringView = () => {
-  const { isLoaded, isFailed } = useTeam();
+  const { isLoaded, isFailed, showGuide } = useTeam();
 
   return (
-    <View isLoaded={isLoaded} isFailed={isFailed} fallback='info' style={{ width: '100%', height: '100%' }}>
+    <View isLoaded={isLoaded} isFailed={isFailed} fallback='info' fallbackAction={showGuide} style={{ width: '100%', height: '100%' }}>
       <ScoringBox />
     </View>
   );
